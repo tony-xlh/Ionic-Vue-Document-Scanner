dfbe56c35446522c5579826497cf3fee020bef7f
xulihang <xulihanghai@163.com>
2022-12-05 17:51:21 +0800

    remove unused styles and ion-header under ion-content

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -19,11 +19,6 @@
     </ion-header>
     
     <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">Blank</ion-title>
-        </ion-toolbar>
-      </ion-header>
       <DWT width="100%" height="100%" @onWebTWAINReady="onWebTWAINReady"></DWT>
       <ion-fab vertical="bottom" horizontal="start" slot="fixed">
         <ion-fab-button @click="startScan">
@@ -157,31 +152,4 @@ export default defineComponent({
 </script>
 
 <style scoped>
-#container {
-  text-align: center;
-  
-  position: absolute;
-  left: 0;
-  right: 0;
-  top: 50%;
-  transform: translateY(-50%);
-}
-
-#container strong {
-  font-size: 20px;
-  line-height: 26px;
-}
-
-#container p {
-  font-size: 16px;
-  line-height: 22px;
-  
-  color: #8c8c8c;
-  
-  margin: 0;
-}
-
-#container a {
-  text-decoration: none;
-}
 </style>
