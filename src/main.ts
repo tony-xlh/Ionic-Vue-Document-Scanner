2c7b3d71a5d6d2ae2f7512bb5c587891c42b99e7
xulihang <xulihanghai@163.com>
2022-12-06 14:00:36 +0800

    uncomment

--- src/main.ts
+++ src/main.ts
@@ -22,7 +22,7 @@ import '@ionic/vue/css/display.css';
 
 /* Theme variables */
 import './theme/variables.css';
-//import './registerServiceWorker';
+import './registerServiceWorker';
 
 const app = createApp(App)
   .use(IonicVue)
