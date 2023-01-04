ffbe0cd8ca4b0b5a569cb04e967e804f8e2b036b
xulihang <xulihanghai@163.com>
2023-01-04 11:20:40 +0800

    prefetch dwt resources in home page

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -12,9 +12,9 @@
 </template>
 
 <script lang="ts">
-import { IonContent, IonHeader, IonButton, IonPage, IonTitle, IonToolbar, IonFab,IonFabButton,IonIcon, actionSheetController, isPlatform, useIonRouter } from '@ionic/vue';
+import { IonContent, IonHeader, IonButton, IonPage, IonTitle, IonToolbar, IonFab,IonFabButton,IonIcon, actionSheetController, isPlatform, useIonRouter,loadingController  } from '@ionic/vue';
 
-import { defineComponent } from 'vue';
+import { defineComponent, onMounted } from 'vue';
 
 export default defineComponent({
   name: 'HomePage',
@@ -31,6 +31,40 @@ export default defineComponent({
     const goToScanner = () => {
       router.push('/scanner');
     }
+
+    const prefetch = async () => {
+      let urls = 
+      ['/assets/dwt-resources/src/dynamsoft.webtwain.css?t=20220624191623',
+        '/assets/dwt-resources/src/dynamsoft.viewer.css?t=20220624191623',
+        '/assets/dwt-resources/src/dynamsoft.viewer.js?t=20220624191623',
+        '/assets/dwt-resources/src/dynamsoft.lts.js?t=20220624191623',
+        '/assets/dwt-resources/addon/dynamsoft.webtwain.addon.camera.css?t=20220624191623',
+        '/assets/dwt-resources/src/dynamsoft.imageio_wasm-1.7.3.js',
+        '/assets/dwt-resources/src/dynamsoft.imageio-1.7.3.wasm',
+        '/assets/dwt-resources/src/dynamsoft.crypto-1.7.3.wasm',
+        '/assets/dwt-resources/src/dynamsoft.imagecore-1.7.3.wasm',
+        '/assets/dwt-resources/src/dynamsoft.imageProc-sn-1.7.3.wasm',
+        '/assets/dwt-resources/src/dynamsoft.pdfReader_wasm-1.7.3.js',
+        '/assets/dwt-resources/src/dynamsoft.pdfReader-1.7.3.wasm',
+      ];
+
+      const loading = await loadingController.create({
+        message: 'Loading resources...',
+      });
+
+      loading.present();
+
+      for (let index = 0; index < urls.length; index++) {
+        const url = urls[index];
+        await fetch(url); 
+      }
+
+      loading.dismiss();
+    }
+
+    onMounted(() => {
+      prefetch();
+    })
 
     return {
       goToScanner
