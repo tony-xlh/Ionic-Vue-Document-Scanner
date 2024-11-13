6a39e102546e6c14ffe69c4044cfd439872c9f5e
xulihang <xulihanghai@163.com>
2024-11-13 11:09:01 +0800

    enable pwa

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import vue from '@vitejs/plugin-vue'
 import path from 'path'
 import { defineConfig } from 'vite'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -18,6 +19,7 @@ export default defineConfig({
       }
     }),
     vueJsx(),
+    VitePWA({ registerType: 'autoUpdate' }),
     legacy()
   ],
   resolve: {
